3de81717cd031c658af95f485a0e55133cb99ba3
hoanggghck <hoanggghck@gmail.com>
2019-04-26 19:09:22 +0700

    thay doi cac component trong module home

---
+++ src/app/home/formlienhe/formlienhe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formlienhe',
+  templateUrl: './formlienhe.component.html',
+  styleUrls: ['./formlienhe.component.scss']
+})
+export class FormlienheComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,58 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutHomeComponent } from './layout-home.component';
-import { TrangChuComponent } from './trang-chu/trang-chu.component';
-import { TrangChiTietComponent } from './trang-chi-tiet/trang-chi-tiet.component';
+import { DienAnhComponent } from './dien-anh/dien-anh.component';
+import { FormlienheComponent } from './formlienhe/formlienhe.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { ItemPhimComponent } from './item-phim/item-phim.component';
+import { KhuyenMaiComponent } from './khuyen-mai/khuyen-mai.component';
+import { LienHeComponent } from './lien-he/lien-he.component';
+import { LoaiPhimComponent } from './loai-phim/loai-phim.component';
+import { PhimDangChieuComponent } from './phim-dang-chieu/phim-dang-chieu.component';
+import { PhimSapChieuComponent } from './phim-sap-chieu/phim-sap-chieu.component';
+import { ReviewComponent } from './review/review.component';
+import { SliderComponent } from './slider/slider.component';
+import { ThongTinLienHeComponent } from './thong-tin-lien-he/thong-tin-lien-he.component';
+import { TinTucComponent } from './tin-tuc/tin-tuc.component';
+
+
 
 @NgModule({
-  declarations: [LayoutHomeComponent, TrangChuComponent, TrangChiTietComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    LayoutHomeComponent,
+    DienAnhComponent,
+    FormlienheComponent,
+    HeaderComponent,
+    ItemPhimComponent,
+    KhuyenMaiComponent,
+    LienHeComponent,
+    LoaiPhimComponent,
+    PhimDangChieuComponent,
+    PhimSapChieuComponent,
+    ReviewComponent,
+    SliderComponent,
+    ThongTinLienHeComponent,
+    TinTucComponent ],
   imports: [
-    CommonModule
+    CommonModule,
+    
+  ],
+  exports: [
+    LayoutHomeComponent,
+    DienAnhComponent,
+    FormlienheComponent,
+    HeaderComponent,
+    ItemPhimComponent,
+    KhuyenMaiComponent,
+    LienHeComponent,
+    LoaiPhimComponent,
+    PhimDangChieuComponent,
+    PhimSapChieuComponent,
+    ReviewComponent,
+    SliderComponent,
+    ThongTinLienHeComponent,
+    TinTucComponent
+    
   ]
 })
 export class HomeModule { }
